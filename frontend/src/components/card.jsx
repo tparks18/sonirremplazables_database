ce41f0e383d82353259cc52ac4a86d1b931bbffc
Taty <tatyanaparks@gmail.com>
2023-05-08 16:24:41 -0500

    added bootstrap script tags

--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import picture from '../assets/Img/logo.png'
 import { person } from "./person";
-import { SearchValue } from "./mainNavbar";
+import { SearchValue } from "./SearchBar";
 import { click } from "@testing-library/user-event/dist/click";
 
 
