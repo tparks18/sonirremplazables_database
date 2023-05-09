b7d90c8eef72d636f3f5031faa3de36576c949e6
Taty <tatyanaparks@gmail.com>
2023-05-09 10:53:07 -0500

    added icons to cards

--- frontend/src/components/person.js
+++ frontend/src/components/person.js
@@ -9,31 +9,33 @@ function Person({ person }) {
       </a>
       <Card.Body>
         <a href={`/person/${person._id}`}>
-          <Card.Title class="text-center" as="div">
+          <Card.Title className="text-center" as="div">
             <strong>
               <h5>
                 {person.first_name} {person.last_name}
               </h5>
             </strong>
           </Card.Title>
-          <Card.Text as="div">
-            <div className="my-2">
-              <ul>
-                <li class="list-group-item">
-                  <strong>Telefono:</strong> {person.phone_number_of_contact}
-                </li>
-                <li class="list-group-item">
-                  <strong>Last Known Location: </strong>
-                  {person.last_known_location}
-                </li>
-                <li class="list-group-item">
-                  <strong>Desaparición:</strong>{" "}
-                  {person.date_and_time_last_seen}
-                </li>
-              </ul>
-            </div>
-          </Card.Text>
         </a>
+        <Card.Text as="div">
+          <div className="my-2">
+            <ul>
+              <li className="list-group-item">
+                <i className="fa-solid fa-phone"></i>
+                <strong> Telefono:</strong> {person.phone_number_of_contact}
+              </li>
+              <li className="list-group-item">
+                <i className="fa-solid fa-location-dot"></i>
+                <strong> Last Known Location: </strong>
+                {person.last_known_location}
+              </li>
+              <li className="list-group-item">
+                <i className="fa-solid fa-calendar-xmark"></i>
+                <strong> Desaparición:</strong> {person.date_and_time_last_seen}
+              </li>
+            </ul>
+          </div>
+        </Card.Text>
       </Card.Body>
     </Card>
   );
