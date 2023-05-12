4e56618020212e1b4bd7fb3183267a1e50039174
Taty <tatyanaparks@gmail.com>
2023-05-11 20:46:38 -0500

    register user route and view added

--- backend/base/views.py
+++ backend/base/views.py
@@ -12,6 +12,9 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -28,19 +31,7 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 # Create your views here.
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/missingPersons/',
-        '/api/missingPersons/create/',
-        '/api/missingPersons/upload/',
-        '/api/missingPersons/top/',
-        '/api/missingPersons/<id>/',
-        '/api/missingPersons/delete/<id>/',
-        '/api/missingPersons/top/<update>/<id>/',
-        
-    ]
-    return Response(routes)
+
 
 @api_view(['GET'])
 def getMissingPersons(request):
@@ -54,6 +45,23 @@ def getMissingPerson(request, pk):
     serializer = MissingPersonSerializer(
         missingPerson, many=False)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
