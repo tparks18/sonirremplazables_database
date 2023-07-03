c5f11cf14c3cb2e6e2bb366531bb81d3bdff6b63
Taty <tatyanaparks@gmail.com>
2023-07-02 19:45:51 -0500

    protected user list route from non admin users

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,27 +1,33 @@
 import React, { useEffect } from "react";
-//import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Table, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import { listUsers } from '../actions/userActions'
-import { Link } from "react-router-dom";
+import { listUsers } from "../actions/userActions";
 
 function UserListScreen() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const dispatch = useDispatch()
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-    const userList = useSelector(state => state.userList)
-    console.log(userList);
-    const { loading, error, users } = userList
+  const userList = useSelector((state) => state.userList);
+  const { loading, error, users } = userList;
+  const redirect = "/login";
 
-    useEffect(() => {
-        dispatch(listUsers())
-    }, [dispatch])
-
-    const deleteHandler = (id) => {
-        console.log('DELETE:', id)
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      navigate(redirect);
     }
+  }, [navigate, userInfo, redirect, dispatch]);
+
+  const deleteHandler = (id) => {
+    console.log("DELETE:", id);
+  };
 
   return (
     <div>
@@ -34,12 +40,12 @@ function UserListScreen() {
         <Table striped bordered hover responsive className="table-sm">
           <thead>
             <tr>
-            <th>ID</th>
-            <th>FIRST NAME</th>
-            <th>LAST NAME</th>
-            <th>EMAIL</th>
-            <th>IS ADMIN</th>
-            <th>EDIT USER</th>
+              <th>ID</th>
+              <th>FIRST NAME</th>
+              <th>LAST NAME</th>
+              <th>EMAIL</th>
+              <th>IS ADMIN</th>
+              <th>EDIT USER</th>
             </tr>
           </thead>
           <tbody>
@@ -61,7 +67,11 @@ function UserListScreen() {
                     <Button variant="light" className="btn-sm me-2">
                       <i className="fas fa-edit"></i>
                     </Button>
-                    <Button variant="danger" className="btn-sm" onClick={() => deleteHandler(user._id)}>
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => deleteHandler(user._id)}
+                    >
                       <i className="fas fa-trash"></i>
                     </Button>
                   </Link>
