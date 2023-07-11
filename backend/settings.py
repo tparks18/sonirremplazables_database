373790ae647d6b0f706edf59c3823413dd796f5c
Taty <tatyanaparks@gmail.com>
2023-07-10 23:54:45 -0500

    changed settings

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
-                 'sonirremplazablesdatabase-2be9f510e62d.herokuapp.com/']
+                 'sonirremplazablesdatabase-2be9f510e62d.herokuapp.com']
 
 
 # Application definition
