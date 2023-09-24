a23fe4539b7fec9e9736ca722f42cdf8c3259b42
Taty <tatyanaparks@gmail.com>
2023-09-24 17:58:20 -0500

    made change

--- staticfiles/frontend/src/App.js
+++ staticfiles/frontend/src/App.js
@@ -19,7 +19,23 @@ function App() {
     <>
       <BootstrapHeader />
 
-      ≈
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
+            <Route path="/person/:id" element={<PersonScreen />} />
+            <Route path="/admin/userlist" element={<UserListScreen />} />
+            <Route path="/admin/user/edit/:id" element={<EditUserScreen />} />
+            <Route path="/admin/personlist" element={<PersonListScreen />} />
+            <Route
+              path="/admin/person/edit/:person_id"
+              element={<EditPersonScreen />}
+            />
+          </Routes>
+        </Container>
       </main>
       <Footer />
     </>
